b36720cb3630fcd3a7a6363c092316a4d907e00d
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2025-12-06 08:06:33 -0800

    storage

---
+++ src/utils/LocalStorage.js
@@ -0,0 +1,21 @@
+export function getComplaints() {
+  return JSON.parse(localStorage.getItem("complaints") || "[]");
+}
+
+export function saveComplaint(data) {
+  const all = getComplaints();
+  all.push(data);
+  localStorage.setItem("complaints", JSON.stringify(all));
+}
+
+export function updateComplaint(index, newData) {
+  const all = getComplaints();
+  all[index] = newData;
+  localStorage.setItem("complaints", JSON.stringify(all));
+}
+
+export function deleteComplaint(index) {
+  const all = getComplaints();
+  all.splice(index, 1); // Remove specific complaint
+  localStorage.setItem("complaints", JSON.stringify(all));
+}
