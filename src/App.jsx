28dc15582cb84826d3eca898d99e7e4cbf4c702f
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-03-13 15:47:18 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-// Zose Providers dukeneye
 import { LanguageProvider } from './contexts/LanguageContext';
 import { ComplaintProvider } from './contexts/ComplaintContext'; 
-// ✅ Import neza Admin Auth Provider (line 6)
 import { AdminAuthProvider } from './contexts/AdminAuthContext'; 
 
-// Components na Pages
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
@@ -18,10 +14,8 @@ import AdminPage from './pages/AdminPage';
 function App() {
     return (
         <Router>
-            {/* Providers zigomba gutangira hejuru */}
             <LanguageProvider>
                 <ComplaintProvider> 
-                    {/* ✅ ADMINAUTHPROVIDER yongeyemo neza */}
                     <AdminAuthProvider>
                         <div className="App">
                             <Navbar />
